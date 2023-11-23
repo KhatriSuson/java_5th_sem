3e04553435f4ace30c9d0fc899834d7ba422d85a
KhatriSuson <susonkhatri110@gmail.com>
2023-11-23 13:27:51 +0545

    I Command Line Args

---
+++ I_Command_Line_Arguments/a_command_line_argument.java
@@ -0,0 +1,8 @@
+package I_Command_Line_Arguments;
+
+public class a_command_line_argument {
+	public static void main(String[] args) {
+
+		System.out.println("Hello" + args[0]);
+	}
+}
---
+++ I_Command_Line_Arguments/b_command_line_argument.java
@@ -0,0 +1,11 @@
+package I_Command_Line_Arguments;
+
+public class b_command_line_argument {
+    public static void main(String[] args) {
+        System.out.println("Sanothimi Campus");
+        for(int i = 0; i < args.length; i++){
+            System.out.println((args[i]));
+        }
+    }
+    
+}
